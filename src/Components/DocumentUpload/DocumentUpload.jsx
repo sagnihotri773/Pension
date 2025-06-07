b808ce3d242731466d9f53f77763b79baa822119
Pankaj Keshta <kashta@gmail.com>
2025-06-07 16:44:59 +0530

    changes

--- src/Components/DocumentUpload/DocumentUpload.jsx
+++ src/Components/DocumentUpload/DocumentUpload.jsx
@@ -76,6 +76,22 @@ const DocumentUpload = () => {
 
   return (
     <div className="document-upload-container">
+      <span className="back-arrow">
+        {" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="35"
+          height="35"
+          fill="currentColor"
+          class="bi bi-arrow-left-short"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"
+          />
+        </svg>
+      </span>
       <h2>Pension Plan Data Management</h2>
       <h2 className="section-title">Document Upload</h2>
 
@@ -136,7 +152,6 @@ const DocumentUpload = () => {
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
-
             </select>
           </div>
 
@@ -169,9 +184,6 @@ const DocumentUpload = () => {
                 value={planId}
                 onChange={(e) => setPlanId(e.target.value)}
               />
-              <button type="button" className="search-button">
-                🔍
-              </button>
             </div>
           </div>
 
@@ -184,9 +196,6 @@ const DocumentUpload = () => {
                 value={participantId}
                 onChange={(e) => setParticipantId(e.target.value)}
               />
-              <button type="button" className="search-button">
-                🔍
-              </button>
             </div>
           </div>
 
@@ -216,54 +225,68 @@ const DocumentUpload = () => {
             ) : (
               <div className="file-container">
                 {uploadedFiles?.map((file) => (
-                  <div className="file-item" key={file.id}>
-                    <div className="file-content">
-                      <div className="file-icon ts">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
+                  <>
+                    <div className="file-item" key={file.id}>
+                      <div className="files">
+                        <div className="file-content">
+                          <div className="file-icon ts">
+                            <svg
+                              fill="none"
+                              stroke="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                              ></path>
+                            </svg>
+                          </div>
+                          <div className="file-details">
+                            <h3>{file.name}</h3>
+                          </div>
+                        </div>
+
+                        <button
+                          className="remove-btn"
+                          onClick={() => onRemove(file.id)}
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                          ></path>
-                        </svg>
+                          <svg
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                          </svg>
+                        </button>
                       </div>
-                      <div className="file-details">
-                        <h3>{file.name}</h3>
-                        <p>({(file.size / 1024 / 1024).toFixed(2)} MB)</p>
-                      </div>
+                      <p className="file-size">
+                        File Size :{(file.size / 1024 / 1024).toFixed(2)} MB
+                      </p>
                     </div>
-                    <button
-                      className="remove-btn"
-                      onClick={() => onRemove(file.id)}
-                    >
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        ></path>
-                      </svg>
-                    </button>
-                  </div>
+                  </>
                 ))}
-                <strong>Total File Size:</strong>{" "}
-                {uploadedFiles
-                  ?.reduce((acc, f) => acc + parseFloat(f.size), 0)
-                  .toFixed(2)}{" "}
-                MB
               </div>
             )}
           </div>
+          <div className="submit-button-container">
+            <button type="button" className="action-button submit-button">
+              Submit
+            </button>
+          </div>
+          <p className="total-size">
+            Total File Size:
+            {uploadedFiles
+              ?.reduce((acc, f) => acc + parseFloat(f.size), 0)
+              .toFixed(2)}{" "}
+            MB
+          </p>
         </div>
       </div>
     </div>
