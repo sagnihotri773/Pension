5e0f57779f1ecd1d6a9b2bc9cc3f00f63053ec08
Pankaj Keshta <you@example.com>
2025-06-11 10:25:35 +0530

    chnages

--- src/Utils/InfoModal.jsx
+++ src/Utils/InfoModal.jsx
@@ -8,31 +8,39 @@ import {
   BsExclamationTriangle,
 } from "react-icons/bs";
 import "./modal.css";
+import { CloseIconSvg } from "./Svg";
 
 const typeConfig = {
   success: {
-    icon: <BsCheckCircle className="text-success" size={22} />,
+    icon: <BsCheckCircle className="text-success" size={25} />,
     title: "Success",
     variant: "success",
     labelClass: "info-modal-type-success",
+    iconColor: "success-icon",
   },
   error: {
     icon: <BsXCircle className="text-danger" size={22} />,
     title: "Error",
     variant: "danger",
     labelClass: "info-modal-type-error",
+    iconColor: "error-icon",
+
   },
   info: {
     icon: <BsInfoCircle className="text-primary" size={22} />,
     title: "Information",
     variant: "primary",
     labelClass: "info-modal-type-info",
+    iconColor: "info-icon",
+
   },
   warning: {
     icon: <BsExclamationTriangle className="text-warning" size={22} />,
     title: "Warning",
     variant: "warning",
     labelClass: "info-modal-type-warning",
+    iconColor: "warning-icon",
+
   },
 };
 
@@ -48,15 +56,18 @@ function InfoModal({ show, type = "info", title, message, onClose }) {
       dialogClassName="info-modal-top-center"
       centered={false}
     >
-      <Modal.Header className={config.labelClass}>
+      <Modal.Header className={config.labelClass} >
         <Modal.Title className="d-flex align-items-center gap-2">
           {config.icon}
           {title || config.title}
         </Modal.Title>
+        <div className="ms-auto cursor-pointer" onClick={onClose}>
+          <CloseIconSvg className={config.labelClass} />
+        </div>
       </Modal.Header>
       <Modal.Body>
         {" "}
-        {config.icon} &nbsp;
+        {React.cloneElement(config.icon, { className: config.iconColor })} &nbsp;
         {message || "Something happened."}
       </Modal.Body>
       <Modal.Footer>
